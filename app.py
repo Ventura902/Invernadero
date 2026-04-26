b6d30f11fe5927136f8cbbca3066115873952424
Ventura902 <cventurad1@miumg.edu.gt>
2026-04-26 17:59:05 -0600

    app.py

--- app.py
+++ app.py
@@ -16,15 +16,15 @@ def convertir_humedad(valor):
     except:
         return 0
 
-url = f"https://api.thingspeak.com/channels/3358332/feeds/last.json?nocache={time.time()}"
+placeholder = st.empty()
 
-try:
-    response = requests.get(url, timeout=5)
-
-    if response.status_code == 200:
-        data = response.json()
+while True:
+    try:
+        url = "https://api.thingspeak.com/channels/3358332/feeds/last.json"
+        data = requests.get(url).json()
 
         if data and data.get("field1"):
+
             h1 = data["field1"]
             h2 = data["field2"]
             temp = float(data["field3"])
@@ -33,55 +33,26 @@ try:
             h1p = convertir_humedad(h1)
             h2p = convertir_humedad(h2)
 
-            col1, col2 = st.columns(2)
+            with placeholder.container():
 
-            col1.metric("🌡️ Temperatura", f"{temp} °C")
-            col1.metric("❄️🔥 Humedad Aire", f"{humA} %")
+                col1, col2 = st.columns(2)
 
-            col2.metric("🌱 Primer Nivel", f"{h1p} %")
-            col2.metric("🌱 Segundo Nivel", f"{h2p} %")
+                col1.metric("🌡️ Temperatura", f"{temp} °C")
+                col1.metric("❄️🔥 Humedad Aire", f"{humA} %")
 
-            st.subheader("❄️🔥 Estado del ambiente")
+                col2.metric("🌱 Primer Nivel", f"{h1p} %")
+                col2.metric("🌱 Segundo Nivel", f"{h2p} %")
 
-            if humA < 40:
-                st.error("❌ Aire seco (baja humedad)")
-            elif humA <= 70:
-                st.success("✅ Humedad ideal")
-            else:
-                st.warning("⚠️ Aire muy húmedo")
+                st.subheader("❄️🔥 Estado del ambiente")
 
-            st.subheader("🌱 Estado del suelo")
+                if humA < 40:
+                    st.error("❌ Aire seco")
+                elif humA <= 70:
+                    st.success("✅ Humedad ideal")
+                else:
+                    st.warning("⚠️ Aire muy húmedo")
 
-            if h1p < 30:
-                st.error("⚠️ Primer Nivel seco")
-            elif h1p < 60:
-                st.warning("🌤️ Primer Nivel medio")
-            else:
-                st.success("💧 Primer Nivel húmedo")
+    except:
+        st.warning("⏳ Esperando datos...")
 
-            if h2p < 30:
-                st.error("⚠️ Segundo Nivel seco")
-            elif h2p < 60:
-                st.warning("🌤️ Segundo Nivel medio")
-            else:
-                st.success("💧 Segundo Nivel húmedo")
-
-            st.markdown(
-                f"<h1 style='text-align: center; font-size: 70px;'>🌡️ {temp} °C</h1>",
-                unsafe_allow_html=True
-            )
-
-            st.markdown(
-                f"<h1 style='text-align: center; font-size: 70px;'>❄️🔥 {humA} %</h1>",
-                unsafe_allow_html=True
-            )
-
-        else:
-            st.warning("⏳ Esperando datos del invernadero...")
-    else:
-        st.error("❌ Error al obtener datos de ThingSpeak")
-
-except:
-    st.error("❌ No se pudo conectar con ThingSpeak")
-
-st.markdown("<meta http-equiv='refresh' content='60'>", unsafe_allow_html=True)
+    time.sleep(15)
