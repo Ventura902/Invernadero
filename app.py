82548be3a621c16da4eb772c0e9d78c88b7b5dbd
Ventura902 <cventurad1@miumg.edu.gt>
2026-04-26 21:05:55 -0600

    app.py

--- app.py
+++ app.py
@@ -5,14 +5,14 @@ import time
 st.set_page_config(page_title="Invernadero", layout="wide")
 st.title("🌿 Sistema Inteligente de Invernadero")
 
+FIREBASE_URL = "https://invernadero-f2926-default-rtdb.firebaseio.com/invernadero.json"
+
 def convertir_humedad(valor):
     seco = 830
-    humedo = 415    
+    humedo = 415
     try:
-        valor = int(float(valor))
         porcentaje = (seco - valor) * 100 / (seco - humedo)
-        porcentaje = max(0, min(100, porcentaje))
-        return round(porcentaje, 1)
+        return max(0, min(100, round(porcentaje, 1)))
     except:
         return 0
 
@@ -20,15 +20,13 @@ placeholder = st.empty()
 
 while True:
     try:
-        url = "https://api.thingspeak.com/channels/3358332/feeds/last.json"
-        data = requests.get(url).json()
+        data = requests.get(FIREBASE_URL).json()
 
-        if data and data.get("field1"):
-
-            h1 = data["field1"]
-            h2 = data["field2"]
-            temp = float(data["field3"])
-            humA = float(data["field4"])
+        if data:
+            h1 = data["h1"]
+            h2 = data["h2"]
+            temp = data["temp"]
+            humA = data["humA"]
 
             h1p = convertir_humedad(h1)
             h2p = convertir_humedad(h2)
@@ -45,14 +43,40 @@ while True:
 
                 st.subheader("❄️🔥 Estado del ambiente")
 
-                if humA < 20:
+                if humA < 40:
                     st.error("❌ Aire seco")
                 elif humA <= 70:
                     st.success("✅ Humedad ideal")
                 else:
                     st.warning("⚠️ Aire muy húmedo")
 
+                st.subheader("🌱 Estado del suelo")
+
+                if h1p < 30:
+                    st.error("⚠️ Primer Nivel seco")
+                elif h1p < 60:
+                    st.warning("🌤️ Primer Nivel medio")
+                else:
+                    st.success("💧 Primer Nivel húmedo")
+
+                if h2p < 30:
+                    st.error("⚠️ Segundo Nivel seco")
+                elif h2p < 60:
+                    st.warning("🌤️ Segundo Nivel medio")
+                else:
+                    st.success("💧 Segundo Nivel húmedo")
+
+                st.markdown(
+                    f"<h1 style='text-align: center; font-size: 70px;'>🌡️ {temp} °C</h1>",
+                    unsafe_allow_html=True
+                )
+
+                st.markdown(
+                    f"<h1 style='text-align: center; font-size: 70px;'>❄️🔥 {humA} %</h1>",
+                    unsafe_allow_html=True
+                )
+
     except:
         st.warning("⏳ Esperando datos...")
 
-    time.sleep(15)
+    time.sleep(1)  # 🔥 actualización cada 1 segundo
